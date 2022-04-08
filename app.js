110d62e57b0f403196b364138cf5529fc3be9942
Michael Brewer <mbrewer@protonmail.com>
2022-04-07 23:39:27 -0500

    Initial commit

--- app.js
+++ app.js
@@ -1,26 +1,21 @@
 // Setup localhost SSL
 // https://www.kevinleary.net/self-signed-trusted-certificates-node-js-express-js/
+// https://support.apple.com/guide/keychain-access/change-the-trust-settings-of-a-certificate-kyca11871/mac
 
+/** Const Vars */
 const https = require('https');
 const fs = require('fs');
-//const express = require('express');
 
 const options = {
 	key: fs.readFileSync('./localhost.key'),
-	cert: fs.readFileSync('./localhost.cert'),
-	requestCert: false,
-	rejectUnauthorized: false
+	cert: fs.readFileSync('./localhost.cert')
+	//requestCert: false,
+	//rejectUnauthorized: false
 };
 
-//const app = express();
-//const port = process.env.PORT || 443;
-//const server = https.createServer( options, app );
-
-//server.listen( port, function () {
-//	console.log('Express Server listening on port ' + server.address().port );
-//});
-
-https.createServer(options, function (req, res) {
+const server = https.createServer(options, function (req, res) {
 	res.writeHead(200);
-	res.end("Hello VR Peeps\n");
+	res.end("VR Peeps\n");
 }).listen(443);
+
+console.log('Server listening on port 443');
