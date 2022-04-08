65d931a41a4035ee9d2833981876640ede3386b5
Michael Brewer <mbrewer@protonmail.com>
2022-04-07 23:02:43 -0500

    Initial commit

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
+// Setup localhost SSL
+// https://www.kevinleary.net/self-signed-trusted-certificates-node-js-express-js/
+
 const https = require('https');
 const fs = require('fs');
+//const express = require('express');
 
 const options = {
-	key: fs.readFileSync('key.pem'),
-	cert: fs.readFileSync('cert.pem')
+	key: fs.readFileSync('./localhost.key'),
+	cert: fs.readFileSync('./localhost.cert'),
+	//requestCert: false,
+	//rejectUnauthorized: false
 };
+
+//const app = express();
+//const port = process.env.PORT || 443;
+//const server = https.createServer( options, app );
+
+//server.listen( port, function () {
+//	console.log('Express Server listening on port ' + server.address().port );
+//});
 
 https.createServer(options, function (req, res) {
 	res.writeHead(200);
 	res.end("Hello VR Peeps\n");
-}).listen(5001);
+}).listen(443);
